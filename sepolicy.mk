db78d53b1b5ecbba3a37ba21a23d1e63d07862ea
rsuntk <rissu.ntk@gmail.com>
2026-07-18 16:06:42 +0700

    SamsungParts: Create sepolicy.mk
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

---
+++ sepolicy.mk
@@ -0,0 +1,5 @@
+SEC_PARTS_PATH := packages/apps/SamsungParts
+
+# SEPolicy
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(SEC_PARTS_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(SEC_PARTS_PATH)/sepolicy/public

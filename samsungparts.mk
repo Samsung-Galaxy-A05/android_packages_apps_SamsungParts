5bd6e40ce4acb5cbad24402bd7175e2c6ba6b2fb
rsuntk <rissu.ntk@gmail.com>
2026-07-18 17:49:54 +0700

    SamsungParts: Include soong namespaces
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- samsungparts.mk
+++ samsungparts.mk
@@ -1,5 +1,8 @@
 SEC_PARTS_PATH := packages/apps/SamsungParts
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += $(SEC_PARTS_PATH)
+
 # SEPolicy
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(SEC_PARTS_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(SEC_PARTS_PATH)/sepolicy/public

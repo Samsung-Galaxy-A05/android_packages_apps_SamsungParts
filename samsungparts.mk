c3c5886fffb9f2d36833994b433ec70f93f29fe2
Smiley9000 <horoidrom@gmail.com>
2026-07-18 17:30:39 +0700

    SamsungParts: Import .rc file
    * Act as helper
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- samsungparts.mk
+++ samsungparts.mk
@@ -7,4 +7,5 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(SEC_PARTS_PATH)/sepolicy/public
 # Samsung Parts
 PRODUCT_PACKAGES += \
     SamsungParts \
+    init.sec-battery.service.rc \
     privapp-permissions-samsungparts.xml
